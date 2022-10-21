53fe738407632741c9a4227efabaabaf1c4011a8
messiahhh <616348105@qq.com>
2022-10-21 19:24:12 +0800

    fix: init

---
+++ README.md
@@ -0,0 +1,22 @@
+# Reactive-Resume-Pdf-Generator
+
+[Reactive-Resume](https://github.com/AmruthPillai/Reactive-Resume)是一款使用友好的简历生成工具，支持页面在线预览以及PDF导出功能。但不知为何官方导出的PDF中汉字字体和预览时的字体不一致（可能是后端机器中不存在该汉字字体？），本工具通过本地爬取预览页面从而生成PDF文件。
+
+
+
+## 安装
+
+``` bash
+npm i reactive-resume-pdf-generator -g
+```
+
+
+
+
+
+## 使用
+
+``` bash
+reactive-resume-pdf-generator https://rxresu.me/zh/akara/resume # 替换成你的简历的在线预览地址
+```
+
---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import puppeteer from "puppeteer";
+import { writeFile } from "fs/promises";
+import path from "path";
+import { PDFDocument } from "pdf-lib";
+
+// reactive-resume 在线简历地址
+const url = process.argv[2];
+
+if (!url) { 
+    throw new Error(`请传入形如https://rxresu.me/zh/akara/resume的reactive-resume在线简历地址`)
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(url, { waitUntil: "networkidle0" });
+  await page.waitForSelector("html.wf-active");
+
+  const pageFormat = await page.$$eval("[data-page]", (pages) =>
+    pages[0].getAttribute("data-format")
+  );
+
+  const resumePages = await page.$$eval("[data-page]", (pages) =>
+    pages.map((page, index) => ({
+      pageNumber: index + 1,
+      innerHTML: page.innerHTML,
+      height: page.clientHeight,
+    }))
+  );
+
+  const pdf = await PDFDocument.create();
+
+  for (let index = 0; index < resumePages.length; index++) {
+    await page.evaluate(
+      (page) => (document.body.innerHTML = page.innerHTML),
+      resumePages[index]
+    );
+
+    const buffer = await page.pdf({
+      printBackground: true,
+      height: resumePages[index].height,
+      width: pageFormat === "A4" ? "210mm" : "216mm",
+    });
+
+    const pageDoc = await PDFDocument.load(buffer);
+    const copiedPages = await pdf.copyPages(pageDoc, [0]);
+
+    copiedPages.forEach((copiedPage) => pdf.addPage(copiedPage));
+  }
+
+  await page.close();
+  const buffer = await pdf.save();
+  await writeFile(path.resolve("./resume.pdf"), buffer);
+  console.log("生成生成resume.pdf");
+  await browser.close();
+})();
